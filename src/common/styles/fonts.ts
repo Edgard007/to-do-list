0aeebe4235f38de66b64da4ac8b57b01f0d401f6
Edgardo Palacios <edgard07hp@gmail.com>
2023-11-01 17:53:04 -0600

    💄 Font family imports

--- src/common/styles/fonts.ts
+++ src/common/styles/fonts.ts
@@ -4,18 +4,25 @@ const Fonts = css`
   // -> Import fonts
   @font-face {
     font-family: "Montserrat";
-    src: url("https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700&display=swap");
+    font-style: normal;
+    font-weight: 100 200 400 500 600 700;
+    src: url("https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;400;500;600;700&display=swap&proxy");
   }
 
   @font-face {
     font-family: "Lora";
+    font-style: normal;
+    font-weight: 400 500 600 700;
     src: url("https://fonts.googleapis.com/css2?family=Lora:wght@400;500;600;700&display=swap");
   }
 
   @font-face {
     font-family: "Hind Madurai";
+    font-style: normal;
+    font-weight: 300 400 500 600 700;
     src: url("https://fonts.googleapis.com/css2?family=Hind+Madurai:wght@300;400;500;600;700&display=swap");
   }
+
 
   --primary-font: "Montserrat", Arial, sans-serif;
   --secundary-font: "Lora", Arial, sans-serif;
