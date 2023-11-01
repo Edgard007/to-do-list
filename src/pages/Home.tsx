f4425e34a85161141148d257eaeb166e99d074b4
Edgardo Palacios <edgard07hp@gmail.com>
2023-11-01 15:28:23 -0600

    🚧 Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,16 +2,36 @@ import { useTranslation } from "react-i18next";
 
 import styled from "styled-components";
 
-// ==> Context
+// ==> Components
+import Input from "@components/Input";
+import Button from "@src/components/Button";
 
 const Home = () => {
   const { t } = useTranslation();
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const { elements } = e.currentTarget;
+
+    const input = elements.namedItem("input");
+    const isInput = input instanceof HTMLInputElement;
+    if (!isInput || input == null) return;
+
+    input.value = "";
+  };
+
   return (
     <Wrapper>
-      <section>
-        <h1>{t("app_name")}</h1>
-        <p>{t("app_descrip")}</p>
+      <section className="first_section">
+        <article>
+          <h1>{t("app_name")}</h1>
+          <p>{t("app_descrip")}</p>
+        </article>
+        <form onSubmit={handleSubmit}>
+          <Input label={t("form_input")} type="text" />
+          <Button text={t("form_btn")} type="submit" />
+        </form>
       </section>
       <section>
         <h1>{t("app_name")}</h1>
@@ -30,6 +50,41 @@ const Wrapper = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   grid-gap: 20px;
+
+  .first_section {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+
+    article,
+    form {
+      width: 100%;
+      height: 25%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+    }
+
+    article {
+      h1 {
+        font-size: var(--font-bg);
+        font-weight: var(--bold-weight);
+        font-family: var(--primary-font);
+        color: var(--color-secundary);
+      }
+
+      p {
+        font-size: var(--font-sm);
+        font-weight: var(--bold-weight);
+        font-family: var(--secundary-font);
+        color: var(--color-secundary-gray);
+      }
+    }
+  }
 
   @media (max-width: 800px) {
     grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
