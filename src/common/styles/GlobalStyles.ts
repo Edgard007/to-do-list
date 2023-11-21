e6fcffc84d8e880f3cd1a97b3ac2b3e4bc9601eb
Edgardo Palacios <edgard07hp@gmail.com>
2023-11-21 16:58:24 -0600

    💄 Update global styles transition properties

--- src/common/styles/GlobalStyles.ts
+++ src/common/styles/GlobalStyles.ts
@@ -13,8 +13,10 @@ export const GlobalStyles = createGlobalStyle`
       margin: 0px;
       padding: 0px;
       font-family: var(--font-main);
-      transition: var(--slow-animation) background-color, var(--slow-animation) color;
       background-color: var(--color-primary);
+      transition-property: background-color, color, transform, visibility;
+      transition-duration: 0.3s;
+      transition-timing-function: ease-in, cubic-bezier(0.6,0.2,0.4,1.5), linear;
   }
 
   ::-webkit-scrollbar {
